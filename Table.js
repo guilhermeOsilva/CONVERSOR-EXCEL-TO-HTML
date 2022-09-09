3db5e9caf21e9c1266a427f3a2b0f1de0926e02b
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-09-09 00:34:15 -0300

    criação de contagem de colunas e linhas

--- Table.js
+++ Table.js
@@ -1,9 +1,16 @@
 class Table {
-    constructor(arr) {
-        this.header = arr[0];
-        arr.shift()
-        this.rows = arr;
-    }
+  constructor(arr) {
+    this.header = arr[0];
+    arr.shift();
+    this.rows = arr;
+  }
+  get RowCount() {
+    return this.rows.length;
+  }
+
+  get ColumnCount() {
+    return this.rows.length;
+  }
 }
 
 module.exports = Table;
