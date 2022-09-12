015e8ec59801be3f88dfbfb93936831221c0a1f1
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-09-12 11:00:53 -0300

    Adicionando um callback de erro

--- PDFWriter.js
+++ PDFWriter.js
@@ -2,7 +2,7 @@ var pdf = require("html-pdf");
 
 class PDFWriter {
     static WritePDF(filename, html) {
-        pdf.create(html,{}).toFile(filename);
+        pdf.create(html,{}).toFile(filename, err => console.log(err));
     }
 }
 
