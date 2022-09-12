045f61cb8abcb408ba87ad79ada825d5aada1f97
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-09-12 06:19:42 -0300

    adicionando as classes de Reader, Table, Writer a função principal de conversão

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+const Processor = require("./Processor");
 var Reader = require("./Reader");
+var Table = require("./Table");
+var HtmlParser = require("./HtmlParser");
+var Writer = require("./Writer");
+var PDFWriter = require("./PDFWriter");
 
 var leitor = new Reader();
+var escritor = new Writer();
+
 
 async function main() {
     
    var dados = await leitor.Read("./usuario.csv");
+   var dadosProcessados = Processor.Process(dados);
+   console.log(dadosProcessados);
 
-   console.log(dados);
+   var usuarios = new Table(dadosProcessados);
+
+   console.log(usuarios.RowCount);
+
+   var html = await HtmlParser.Parse(usuarios);
+   console.log(html);
+
+   escritor.Write(Date.now() + ".html", html);
+
 }
+
+
 
 main()
